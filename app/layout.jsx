a1cd0df3705de380bf63370bd0115f10eda34f51
Shad <shadalam0024@gmail.com>
2025-07-24 16:03:08 +0530

    Page animation added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 // layout.js - keep as is
 const jetbrainsMono = JetBrains_Mono({
@@ -22,7 +24,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${jetbrainsMono.variable}  antialiased`}>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
